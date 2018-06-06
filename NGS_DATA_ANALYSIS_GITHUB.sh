ca137cd23ae8cf3b54e9469cbf9c93e7fa14d683
LakshmanTejaG <lakshmanteja@gmail.com>
2018-06-06 17:43:03 +0530

    Github-Upload

---
+++ NGS_DATA_ANALYSIS_GITHUB.sh
@@ -0,0 +1,13 @@
+https://github.com/LakshmanTejaG/NGS_DATA_ANALYSIS.git
+
+##Create a new repository on the command line
+echo "# NGS_DATA_ANALYSIS: A Repository for NGS Data Analysis pipelines- DNA-SEQ | RNA-SEQ | CHIP-SEQ | META-SEQ" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/LakshmanTejaG/NGS_DATA_ANALYSIS.git
+git push -u origin master
+
+##Push an existing repository from the command line
+git remote add origin https://github.com/LakshmanTejaG/NGS_DATA_ANALYSIS.git
+git push -u origin master
